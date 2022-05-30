473837800e8d0f4f30d417c5b8a9b8964b1db14e
Nik Waghchaure <waghchaureniks@gmail.com>
2022-05-31 03:20:46 +0530

    Updates

---
+++ precache-manifest.e782af2e3b4875c8b9dfe6b7edfb57fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0eebeecf7cf4e58e0de0",
+    "url": "/Explore_U-Nik/static/css/main.7f72af80.chunk.css"
+  },
+  {
+    "revision": "0eebeecf7cf4e58e0de0",
+    "url": "/Explore_U-Nik/static/js/main.a0f22de6.chunk.js"
+  },
+  {
+    "revision": "9657914080b739550526",
+    "url": "/Explore_U-Nik/static/js/runtime~main.dda06280.js"
+  },
+  {
+    "revision": "92ab30f9e14bfe49d99a",
+    "url": "/Explore_U-Nik/static/js/2.a8efaf71.chunk.js"
+  },
+  {
+    "revision": "202812f2d12cdcb86b0f88a3cb6d0096",
+    "url": "/Explore_U-Nik/static/media/nik1.202812f2.png"
+  },
+  {
+    "revision": "cc36f0d02259e8b359f2a1c96a8d8f20",
+    "url": "/Explore_U-Nik/index.html"
+  }
+];
